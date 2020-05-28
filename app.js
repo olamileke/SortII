35e7d365829fa805b1c9b19f0b65e54b269460c7
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-05-28 14:26:12 +0100

    Deploy Updates II

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SortII'));
 
-app.get('/*', function(req,res) {
-    
-	res.sendFile(path.join(__dirname+'/dist/SortII/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'dist', 'SortII')));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/', (req, res, next) => {
+
+	res.sendFile(path.join(__dirname, 'dist', 'SortII', 'index.html'));
+})
